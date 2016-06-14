88849e1bfb59cd81b3e2ad8f0091a1c3f33bd206
Eugeny Shingarev <shingarev@api.ai>
2016-06-15 04:31:13 +0600

    [*] demo updated

--- demo/demo.js
+++ demo/demo.js
@@ -1,17 +1,16 @@
 var client = new ApiAi.Client('YOUR_ACCESS_TOKEN');
-
 //will work (well, somehow)
 var request = 'test';
 //will not work, too long, should throw error
 var longTextRequest = 'test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test ';
 
-var promise = client.textRequest(longTextRequest);
-promise.then(
-    function (serverResponse) {
-        console.log(serverResponse);
-    }
-).catch(
-    function (serverError) {
-        console.log(serverError);
-    }
-);
+client.textRequest(longTextRequest)
+    .then(handleResponse)
+    .catch(heandleError);
+
+function handleResponse(serverResponse) {
+    console.log(serverResponse);
+}
+function heandleError(serverError) {
+    console.log(serverError);
+}
